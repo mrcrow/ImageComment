f1d364d0b36cc254e24c7c0b68d7528c55422404
mrcrow <wwz.michael@gmail.com>
2012-12-20 22:20:27 +0800

    Initial Commit

---
+++ ImageComment/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  ImageComment
+//
+//  Created by Wu Wenzhi on 12-12-20.
+//  Copyright (c) 2012年 Wu Wenzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ ImageComment/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  ImageComment
+//
+//  Created by Wu Wenzhi on 12-12-20.
+//  Copyright (c) 2012年 Wu Wenzhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
